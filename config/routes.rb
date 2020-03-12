88ff26cb2a241cfd49a9d3238336ccda29067470
ukolovda <udmitry@mail.ru>
2020-03-12 12:51:10 +0400

    Refactoring, tests for Words controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'welcome#index'
+  root to: 'words#index'
 
   get '/autocomplete/words' => 'autocomplete#words'
 end
